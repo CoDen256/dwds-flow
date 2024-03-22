c192086acc0afb9556079685fea539dd683d372b
Denys Chernyshov <denys.chernyshov@optimax-energy.de>
2024-03-22 15:02:59 +0100

    Finish install script

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@ Set-Variable -Name "FLOW_HOME" (Get-ChildItem -Dir -Path "~\scoop\apps\flow-laun
 
 $tag = (Invoke-RestMethod -Uri https://api.github.com/repos/CoDen256/dwds-flow/releases/latest).tag_name
 Invoke-WebRequest https://github.com/CoDen256/dwds-flow/releases/latest/download/DWDS-Flow-$tag.zip -OutFile ~\Downloads\DWDS-Flow-$tag.zip
-Remove-Item "$FLOW_HOME\UserData\Plugins\DWDS-Flow-*" -Recurse
+Remove-Item "$FLOW_HOME\UserData\Plugins\DWDS-Flow-*" -Recurse -Force
 Expand-Archive -Path ~\Downloads\DWDS-Flow-$tag.zip -DestinationPath "$FLOW_HOME\UserData\Plugins\DWDS-Flow-$tag"
 Remove-Item -Path ~\Downloads\DWDS-Flow-$tag.zip
 
