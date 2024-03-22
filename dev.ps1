8e262ebcba4c230be619bbcfec7406802f6af892
Denys Chernyshov <denys.chernyshov@optimax-energy.de>
2024-03-22 15:11:42 +0100

    Finish dev install script

--- dev.ps1
+++ dev.ps1
@@ -1,1 +1,6 @@
 Set-Variable -Name "FLOW_HOME" (Get-ChildItem -Dir -Path "~\scoop\apps\flow-launcher\current" -Filter 'app*').FullName
+Remove-Item -Path "$FLOW_HOME\UserData\Plugins\DWDS-Flow-Dev" -Recurse -Force
+New-Item -Path "$FLOW_HOME\UserData\Plugins\DWDS-Flow-Dev" -ItemType Junction -Value $PWD -Force
+
+taskkill /f /im Flow*
+Start-Process -FilePath "~\scoop\apps\flow-launcher\current\Flow.Launcher.exe"
