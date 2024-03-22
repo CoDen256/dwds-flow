23415f42c4b3812e5157282da5ebcba7ce7cec04
Denys Chernyshov <denys.chernyshov@optimax-energy.de>
2024-03-22 14:59:09 +0100

    Finish install script

--- install.ps1
+++ install.ps1
@@ -1,6 +1,10 @@
 Set-Variable -Name "FLOW_HOME" (Get-ChildItem -Dir -Path "~\scoop\apps\flow-launcher\current" -Filter 'app*').FullName
 
+$tag = (Invoke-RestMethod -Uri https://api.github.com/repos/CoDen256/dwds-flow/releases/latest).tag_name
+Invoke-WebRequest https://github.com/CoDen256/dwds-flow/releases/latest/download/DWDS-Flow-$tag.zip -OutFile ~\Downloads\DWDS-Flow-$tag.zip
+Remove-Item "$FLOW_HOME\UserData\Plugins\DWDS-Flow-*" -Recurse
+Expand-Archive -Path ~\Downloads\DWDS-Flow-$tag.zip -DestinationPath "$FLOW_HOME\UserData\Plugins\DWDS-Flow-$tag"
+Remove-Item -Path ~\Downloads\DWDS-Flow-$tag.zip
 
-Invoke-WebRequest https://github.com/CoDen256/dwds-flow/releases/latest/download/DWDS-Flow.zip -OutFile -OutFile "$FLOW_HOME\UserData\Plugins\DWDS-Flow.zip"
-$githubLatestRelease = Invoke-WebRequest https://api.github.com/repos/CoDen256/dwds-flow/releases/latest | ConvertFrom-Json
-unzip DWDS-Flow.zip
+taskkill /f /im Flow*
+Start-Process -FilePath "~\scoop\apps\flow-launcher\current\Flow.Launcher.exe"
